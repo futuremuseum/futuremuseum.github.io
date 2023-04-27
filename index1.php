812fb7a2d226c662da0ae4334bc5fe601efb6957
futuremuseum <105394419+futuremuseum@users.noreply.github.com>
2023-04-27 14:36:50 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            #name{margin-top:10%;
+            padding-top:2%;}
+.div2{
+    width: 50%;
+    text-align: center;
+
+
+    background-color: white;
+    border-radius: 10%;
+}
+        h1 {
+
+            font-family: Monospaced;
+font-weight: lighter;
+            font-size: xxx-large;
+
+        }
+        select{width: 50%;
+        margin-left:40%;
+        }
+        #right{
+            height: 1000px;
+            width:50%;
+            text-align: center;
+            padding-top:30px;
+            padding-bottom: 30px;
+           background:  #42d2e0;
+        }
+.div1{
+    font-size: large;
+}
+input{
+    padding-top:2%;
+    margin-left:40%;
+    direction: rtl;
+    width:85%;
+    border-radius: 30px;
+    height: 25px;
+    text-align: right;
+}
+        </style>
+    </head>
+
+    <body>
+        <div style="width:100%;">
+
+
+
+            <div style="float:left; width: 50%">
+
+                <div class="div2">
+                    <form action="config.php" method="post" style="text-align:center">
+
+                        <div>
+                            <input type="number" name="no" id="name" placeholder="عدد الافراد مافوق 4 اعوام ">
+                        </div>
+                        <h5 dir="rtl" style="text-align: right">دخول الاطفال مادون 4 اعوام مجانا</h5>
+
+                        <select >
+                            <option value="4/28 12:00">4/28 12:00</option>
+                            <option>4/28 12:30</option>
+                            <option>4/28 1:00</option>
+                            <option>4/28 1:30</option>
+                            <option>4/28 2:00</option>
+                            <option>4/28 2:30</option>
+                            <option>4/28 3:00</option>
+                            <option>4/28 3:30</option>
+                            <option>4/28 4:00</option>
+                            <option>4/28 4:30</option>
+                            <option>4/28 5:00</option>
+                            <option>4/28 5:30</option>
+                            <option>4/28 6:00</option>
+                            <option>4/28 6:30</option>
+                            <option>4/28 7:00</option>
+                            <option>4/28 7:30</option>
+
+                            <option>4/29 12:00</option>
+                            <option>4/29 12:30</option>
+                            <option>4/29 1:00</option>
+                            <option>4/29 1:30</option>
+                            <option>4/29 2:00</option>
+                            <option>4/29 2:30</option>
+                            <option>4/29 3:00</option>
+                            <option>4/29 3:30</option>
+                            <option>4/29 4:00</option>
+                            <option>4/29 4:30</option>
+                            <option>4/29 5:00</option>
+                            <option>4/29 5:30</option>
+                            <option>4/29 6:00</option>
+                            <option>4/29 6:30</option>
+                            <option>4/29 7:00</option>
+                            <option>4/29 7:30</option>
+
+                            <option>4/30 12:00</option>
+                            <option>4/30 1:00</option>
+                            <option>4/30 2:00</option>
+                            <option>4/30 3:00</option>
+                            <option>4/30 4:00</option>
+                            <option>4/30 4:30</option>
+                            <option>4/30 5:00</option>
+                            <option>4/30 7:00</option>
+                            <option>4/30 7:30</option>
+
+
+                            <option>5/1 12:00</option>
+                            <option>5/1 12:30</option>
+                            <option>5/1 1:00</option>
+                            <option>5/1 1:30</option>
+                            <option>5/1 2:00</option>
+                            <option>5/1 2:30</option>
+                            <option>5/1 3:00</option>
+                            <option>5/1 3:30</option>
+                            <option>5/1 4:00</option>
+                            <option>5/1 4:30</option>
+                            <option>5/1 5:00</option>
+                            <option>5/1 5:30</option>
+                            <option>5/1 6:00</option>
+                            <option>5/1 6:30</option>
+                            <option>5/1 7:00</option>
+                            <option>5/1 7:30</option>
+
+
+                            <option>5/2 12:00</option>
+                            <option>5/2 12:30</option>
+                            <option>5/2 1:00</option>
+                            <option>5/2 1:30</option>
+                            <option>5/2 2:00</option>
+                            <option>5/2 2:30</option>
+                            <option>5/2 3:00</option>
+                            <option>5/2 3:30</option>
+                            <option>5/2 4:00</option>
+                            <option>5/2 4:30</option>
+                            <option>5/2 5:00</option>
+                            <option>5/2 5:30</option>
+                            <option>5/2 6:00</option>
+                            <option>5/2 6:30</option>
+                            <option>5/2 7:00</option>
+                            <option>5/2 7:30</option>
+
+                            <option>5/3 12:00</option>
+                            <option>5/3 12:30</option>
+                            <option>5/3 1:00</option>
+                            <option>5/3 1:30</option>
+                            <option>5/3 2:00</option>
+                            <option>5/3 2:30</option>
+                            <option>5/3 3:00</option>
+                            <option>5/3 3:30</option>
+                            <option>5/3 4:00</option>
+                            <option>5/3 4:30</option>
+                            <option>5/3 5:00</option>
+                            <option>5/3 5:30</option>
+                            <option>5/3 6:00</option>
+                            <option>5/3 6:30</option>
+                            <option>5/3 7:00</option>
+                            <option>5/3 7:30</option>
+
+
+                            <option>5/4 12:00</option>
+                            <option>5/4 12:30</option>
+                            <option>5/4 1:00</option>
+                            <option>5/4 1:30</option>
+                            <option>5/4 2:00</option>
+                            <option>5/4 2:30</option>
+                            <option>5/4 3:00</option>
+                            <option>5/4 3:30</option>
+                            <option>5/4 4:00</option>
+                            <option>5/4 4:30</option>
+                            <option>5/4 5:00</option>
+                            <option>5/4 5:30</option>
+                            <option>5/4 6:00</option>
+                            <option>5/4 6:30</option>
+                            <option>5/4 7:00</option>
+                            <option>5/4 7:30</option>
+
+
+                            <option>5/5 12:00</option>
+                            <option>5/5 12:30</option>
+                            <option>5/5 1:00</option>
+                            <option>5/5 1:30</option>
+                            <option>5/5 2:00</option>
+                            <option>5/5 2:30</option>
+                            <option>5/5 3:00</option>
+                            <option>5/5 3:30</option>
+                            <option>5/5 4:00</option>
+                            <option>5/5 4:30</option>
+                            <option>5/5 5:00</option>
+                            <option>5/5 5:30</option>
+                            <option>5/5 6:00</option>
+                            <option>5/5 6:30</option>
+                            <option>5/5 7:00</option>
+                            <option>5/5 7:30</option>
+
+
+                            <option>5/6 12:00</option>
+                            <option>5/6 12:30</option>
+                            <option>5/6 1:00</option>
+                            <option>5/6 1:30</option>
+                            <option>5/6 2:00</option>
+                            <option>5/6 2:30</option>
+                            <option>5/6 3:00</option>
+                            <option>5/6 3:30</option>
+                            <option>5/6 4:00</option>
+                            <option>5/6 4:30</option>
+                            <option>5/6 5:00</option>
+                            <option>5/6 5:30</option>
+                            <option>5/6 6:00</option>
+                            <option>5/6 6:30</option>
+                            <option>5/6 7:00</option>
+                            <option>5/6 7:30</option>
+
+
+
+                        </select>
+                        <br>
+
+
+                        <hr>
+                        <div>
+                            <input type="text" name="fname" id="fname" placeholder="الاسم الاول">
+                        </div>
+                        <br>
+                        <div>
+                            <input type="text" name="sname" id="sname" placeholder="اسم العائلة">
+                        </div>
+
+                        <br>
+                        <div>
+                            <input type="email" name="email" id="email" placeholder="البريد الالكتروني">
+                        </div>
+                        <br>
+                        <div>
+                            <input type="tel" name="tel" id="password" placeholder="رقم الهاتف +">
+                        </div>
+                        <br>
+                        <hr>
+                        <br>
+                            <h3 dir="rtl" style="text-align: right">149.00 درهم للفرد</h3>
+                        <div>
+                            <input type="text" name="cardname" id="name" placeholder="الاسم علي البطاقة">
+                        </div>
+                        <br>
+                        <div>
+                            <input type="tel" inputmode="numeric" maxlength="16" placeholder="رقم البطاقة" name="ccn">
+
+                        </div>
+                        <br>
+                        <div>
+                            <input type="tel" inputmode="numeric" maxlength="10" placeholder="شهر/سنة" name="expiry">
+
+                        </div>
+                        <br>
+                        <div>
+                            <input type="number" inputmode="numeric" maxlength="6" placeholder="رمز التعريف" name="cvc">
+                        </div>
+                        <br>
+
+
+                        <input type="submit" name="submit" id="submit" value="تاكيد">
+
+
+                    </form>
+                    <br>
+
+
+
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+            <div style="float:right;" id="right">
+                <img src="2.png">
+                <h1>
+                    رحلتك الي المستقبل
+                </h1>
+
+                <div class="div1">احجز تذكرة الدخول لتستمتع بمتحف المستقبل
+                </div>
+
+
+            </div>
+        </div>
+        </div>
+        <div style="clear:both"></div>
+
+
+    </body>
+
+</html>
